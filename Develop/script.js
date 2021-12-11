7b13ff7bba17d38c3eb1d2014c3c8ff7b6ff6420
Alan Phelps <phelpsa64@gmail.com>
2021-12-11 16:01:40 -0500

    added to README and added more description on the js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,45 @@
-// character type arrays
+// character type arrays stored in global variables
 var lcChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var ucChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var specChars =["!", "@", "#", "$", "%", "^", "&", "*"];
-
-// Get references to the #generate element
+// get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", function() {
+  // I couldn't get the original way this was written to work with the actual button on the page, so I found that doing the following did the trick after searching for a solution online
   password = writePassword();
   document.querySelector("#password").placeholder = password; 
 });
-
+// function expression for what writes the password
 var writePassword = function() {
+  // generates the length of the password based on user input
   var generatePassword = parseInt(window.prompt("Please input how long you would like your password to be between 8 and 128 characters."));
   if (!generatePassword || generatePassword < 8 || generatePassword > 128) {
     generatePassword = parseInt(window.prompt("Length must be between 8-128 characters. Please try again."));
-
-  } 
-  else {
+  } else {
+    // checks if the user wants to use lowercase letters
     lowerCaseCF = window.confirm("Would you like your password to contain lowercase letters?");
     if (lowerCaseCF) {
       window.alert("Lower case letters will be used.")
     } else {
       window.alert("Lower case letters will not be used.")
     }
-
+    // checks if the user wants to use uppercase letters
     upperCaseCF = window.confirm("Would you like your password to contain uppercase letters?");
     if (upperCaseCF) {
       window.alert("Upper case letters will be used.")
     } else {
       window.alert("Upper case letters will not be used.")
     }
-
+    // checks if the user wants to use numerals
     numeralsCF = window.confirm("Would you like your password to contain numerals?");
     if (numeralsCF) {
       window.alert("Numerals will be used.")
     } else {
       window.alert("Numerals will not be used.")
     }
-
+    // checks if the user wants to use special characters
     specialCharsCF = window.confirm("Would you like your password to contain special characters?");
     if (specialCharsCF) {
       window.alert("Special characters will be used.")
@@ -48,7 +47,6 @@ var writePassword = function() {
       window.alert("Special characters will not be used.")
     }
   };
-
   // if no character types are selected
   if (!lowerCaseCF && !upperCaseCF && !numeralsCF && !specialCharsCF) {
     // "confirmations" keeps track of character type arrays to concatenate based on user input
@@ -88,22 +86,21 @@ var writePassword = function() {
   } else if (specialCharsCF) {
     confirmations = specChars;
   };
-  
   // this empty array will be filled by joining pushed data from the random generator below
   var pwArray = [];
-
+  // randomizer
   for (var i = 0; i < generatePassword; i++) {
     var chooseConfirms = confirmations[Math.floor(Math.random() * confirmations.length)];
     pwArray.push(chooseConfirms);
   }
-
+  // specifies the place in which the password's text will go on the page
   var passwordText = function(password) {
     document.querySelector("#password").textContent = password;
   }
-  
+  // takes what was output by the randomizer and joins it all together into the empty array, "pwArray"
   var password = pwArray.join("");
   passwordText(password);
   return password;
 }
-
+// calls the function to write the password
 writePassword();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # password-generator-challenge
 Password Generator
+
+In this challenge I have created a password generator that allows users to create a password anywhere from 8 to 128 characters in length, using randomized confirmations of lowercase letters, uppercase letters, numerals, and special characters. Users may pick and choose between these character types using window confirm prompts, which are recorded and funneled into the randomizer. The randomizer then pushes its output to an empty array, which becomes the final randomized password requested by the user.
+
+In this challenge I used if statements, a for loop for the randomizer, and had to learn how to join randomized values from different arrays and then push those values to an empty array (thank you to Google). I also had to name variables and assign them either locally to a function or globally, depending on how pervasive I wanted them to be.
